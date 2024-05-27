d46b45a18a4d90cb69b6789ac5d324e7e90d8641
Massart <quentinmassart93@gmail.com>
2024-05-27 20:17:09 +0200

    add speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,5 +2,8 @@ export const prerender = true;
 
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 inject({ mode: dev ? 'development' : 'production' });
+
+injectSpeedInsights();
